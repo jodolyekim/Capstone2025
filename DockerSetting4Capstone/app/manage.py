5b78fd2b204598aa1b7b56fbfc78248b49102db3
jmiisu <94832404+jmiisu@users.noreply.github.com>
2025-05-02 21:23:11 +0900

    Update manage.py

--- DockerSetting4Capstone/app/manage.py
+++ DockerSetting4Capstone/app/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Capstone2025.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DockerSetting4Capstone.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
