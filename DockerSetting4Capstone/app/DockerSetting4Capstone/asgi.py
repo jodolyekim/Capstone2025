389c3d6deea11e36d18b0b5f2cae20b49fc549ae
jmiisu <94832404+jmiisu@users.noreply.github.com>
2025-05-02 21:23:45 +0900

    Update asgi.py

--- DockerSetting4Capstone/app/DockerSetting4Capstone/asgi.py
+++ DockerSetting4Capstone/app/DockerSetting4Capstone/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Capstone2025.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DockerSetting4Capstone.settings')
 
 application = get_asgi_application()
