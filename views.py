c543f2385ee227884d8d51d8b07889f6c866e096
jmiisu <94832404+jmiisu@users.noreply.github.com>
2025-05-08 15:10:46 +0900

    Update views.py

--- views.py
+++ views.py
@@ -9,7 +9,12 @@ from django.core.files.uploadedfile import InMemoryUploadedFile
 # Create your views here.
 class PhotoUploadView(APIView):
     def post(self, request):
+        print("hihi")
         image: InMemoryUploadedFile = request.FILES['image']
+        print(image, image.name)
+
         public_url = upload_file_to_firebase(image, image.name)
+        print(public_url)
+
         photo = Photo.objects.create(image=image, firebase_url=public_url)
         return Response({'firebase_url' : public_url})
