fd9d77c4cf853de085150147c17da2393bf7d64f
jmiisu <94832404+jmiisu@users.noreply.github.com>
2025-05-20 19:55:36 +0900

    Delete DjangoIntegration1st directory

--- DjangoIntegration1st/interest/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Interest, InterestCategory, InterestKeywordCategoryMap
-
-admin.site.register(Interest)
-admin.site.register(InterestCategory)
-admin.site.register(InterestKeywordCategoryMap)
--- DjangoIntegration1st/photos/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import PhotoUploadView
-
-urlpatterns = [
-    path('upload/', PhotoUploadView.as_view(), name='photo-upload'),
-]
