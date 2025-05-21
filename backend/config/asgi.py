495797615cc47b39066a7a8268e3f9ada5ebdc47
jmiisu <94832404+jmiisu@users.noreply.github.com>
2025-05-21 14:01:52 +0900

    Add files via upload

---
+++ backend/config/asgi.py
@@ -0,0 +1,31 @@
+"""
+ASGI config for config project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+import django
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+# ✅ routing은 setup 이후에 import
+import chat.routing
+
+# ✅ ASGI 애플리케이션 정의
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),  # HTTP 요청 처리
+    "websocket": AuthMiddlewareStack(  # WebSocket 요청 처리
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
